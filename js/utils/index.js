fe961b8176673322d46a9e1fa20ef01d0e473f28
Sevara <nematullayevasevara6@gamil.com>
2025-01-16 17:54:34 +0500

    little change

--- js/utils/index.js
+++ js/utils/index.js
@@ -27,3 +27,40 @@ const useFetch = () => {
 };
 
 export { useFetch };
+
+function addUIData(value, carts) {
+  let card = document.createElement("div");
+  card.innerHTML = `
+    <div class="flex flex-col gap-[10px]">
+        <div>
+          <img
+            src="https://images.uzum.uz/crm0jeuvip07shn579rg/t_product_540_high.jpg#1736222803923"
+            alt=""
+          />
+        </div>
+
+        <div class="flex flex-col gap-[5px]">
+          <h2 class="text-[14px]">
+            ${value.title}
+          </h2>
+          <p><i class="bx bxs-star text-[gold]"></i>${value.rate}</p>
+          <h2 class="font-[700] text-[18px] text-[#006bff]">${value.price} so'm</h2>
+          <div class="monthly border border-[#fe7300] rounded-[4px] p-[7px]">
+            <h3 class="font-[500] text-[14px] text-[#fe7300]">
+              ${value.month} so'm/oyiga
+            </h3>
+          </div>
+
+          <div class="flex items-center justify-between">
+            <button class="buy">Купить в один клик</button>
+            <div class="shopping_bag">
+              <i class="bx bx-shopping-bag text-2xl text-[white]"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+      `;
+  carts.append(card);
+}
+
+export { addUIData };
